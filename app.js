27998f7df9b0cb9831c62740c2ccb342ccc61efb
RicardoAgk <ricardoandre.dasein@gmail.com>
2020-11-04 10:07:41 +0000

    done

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express(); //represents our server;
+
+app.use(express.static('public'));//necessary to give access to the 'public' folders
+
+
+//creating a route/home
+app.get('/home', (request, response) => {
+    response.sendFile(__dirname + '/views/home-page.html');
+});
+app.get('/about', (request, response) =>{
+    response.sendFile(__dirname + '/views/about.html')
+});
+app.get('/works', (request, response) =>{
+    response.sendFile(__dirname + '/views/works.html')
+});
+app.get('/photo-gallery', (request, response) =>{
+    response.sendFile(__dirname + '/views/photo-gallery.html')
+});
+app.listen(3000, () => {
+    console.log('My first app using express is waiting for requests')
+});
